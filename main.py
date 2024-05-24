67dc22b80139c696919d27e69d1185fe4e8c1306
Jakob Preuer <preuerj170117@sus.htl-grieskirchen.at>
2024-05-24 08:26:15 +0200

    Jobs are written to, db

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
+
 import re
 def get_job_info(url):
 
@@ -11,9 +14,9 @@ def get_job_info(url):
 
     soup = BeautifulSoup(content_as_str, 'html.parser')
 
-    for a in soup.findAll("a",  href=True):
+    for a in soup.find_all("a",  href=True):
         #print(str(a.text.strip()).split("/")[-1])
-        print (str(a['href']).split("/")[-1])
+        #print (str(a['href']).split("/")[-1])
         if a.text:
             if regex_pattern.match(str(a['href']).split("/")[-1]):
                 job_id_and_name.update({str(a['href']).split("/")[-1]: a.text.strip()})
@@ -22,10 +25,52 @@ def get_job_info(url):
 
     return job_id_and_name
 
+def write_to_file(job_id_and_name):
+    with open("job_id" ,"a") as file:
+        for job_ids in job_id_and_name:
+            file.write(f"{job_ids}; {job_id_and_name.get(job_ids)}; \n")
+    return
 
+def create_db(db_connection):
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("CREATE TABLE IF NOT EXISTS jobs (job_id INTEGER PRIMARY KEY, job_name TEXT, date DATETIME);")
+    db_connection.commit()
+
+    return
+
+def write_to_db(db_connection, job_id_and_name):
+    #Check if Dataset is in Database
+    #If not write to DB
+    db_cursor = db_connection.cursor()
+    all_db_entries = get_all_db_entries(db_cursor)
+    rows_to_insert = list()
+    for key in job_id_and_name:
+        print(job_id_and_name.get(key))
+        if int(key) not in all_db_entries: #Entrie not in DB -> write to DB
+            rows_to_insert.append((int(key), job_id_and_name.get(key), datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
+
+    if len(rows_to_insert) > 0:
+        db_cursor.executemany('INSERT INTO jobs values (?,?,?)', rows_to_insert)
+        db_connection.commit()
+    return
+
+def get_all_db_entries(db_cursor):
+    db_cursor.execute("select job_id from jobs")
+    return_list = list()
+    all_job_id = db_cursor.fetchall()
+
+    for i in all_job_id:
+        return_list.append(i[0])
+
+    return return_list
 
 
 if __name__ == "__main__":
-    job_id_and_name = get_job_info("https://www.karriere.at/jobs/ried-im-innkreis-bezirk?jobFields%5B%5D=2172")
+    db_con = sqlite3.connect("karriere.db")
 
-    print(job_id_and_name)
+    job_id_and_name = get_job_info("https://www.karriere.at/jobs/ried-im-innkreis-bezirk?jobFields%5B%5D=2172")
+    #write_to_file(job_id_and_name)
+
+    create_db(db_con)
+    write_to_db(db_con, job_id_and_name)
+    exit(0)
